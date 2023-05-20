d1925b74f18c35f6c3b5d07a1827d9aeb0de2025
Rémi Dubreucq <Kazzouwi59@gmail.com>
2023-05-20 17:40:51 +0200

    Fin reservation

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,19 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\Table;
+
 use App\Entity\Reservation;
 use App\Repository\OpeningHoursRepository;
 use App\Repository\ReservationRepository;
 use App\Repository\TableRepository;
-use DateInterval;
 use DateTime;
-use Doctrine\ORM\EntityManager;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Length;
+
 
 class ReservationController extends AbstractController
 {
@@ -59,7 +58,7 @@ class ReservationController extends AbstractController
         }
 
         if ($tables == false) {
-            return $this->render('reservation/reservatioFull.html.twig', [
+            return $this->render('reservation/reservationFull.html.twig', [
                 'places' => $places
             ]);
         }
@@ -94,29 +93,50 @@ class ReservationController extends AbstractController
         $resetList = $listOfInterval;
 
         $newArray = [];
-        foreach ($tables as $table) {
-            foreach($reservations as $reservation){
-                if ( $reservation->getReservationTable()->getId() == $table->getId()) {
-                    $rHour = $reservation->getHour();
-                    $rHourTimeStamp = $rHour->getTimestamp();
 
-                    $position = array_search($rHourTimeStamp, $listOfInterval);
+        $duplicates = [];
+
+        foreach ($tables as $table) {
+            $tableReservation = $table->getTableReservation();
+            if (count($tableReservation) == 0) {
+                $listOfInterval = $resetList;
+                break;
+            } else {
+                foreach($reservations as $reservation){
+                    if ( $reservation->getReservationTable()->getId() == $table->getId()) {
+                        $rHour = $reservation->getHour();
+                        $rHourTimeStamp = $rHour->getTimestamp();
+
+                        $position = array_search($rHourTimeStamp, $listOfInterval);
 
 
                         if ( $position !== false ) {
                             $startingIndex = max(0, $position - 3);
                             $endingIndex = min(count($listOfInterval) - 1, $position + 3);
 
-                            array_splice($listOfInterval, $startingIndex, $endingIndex - $startingIndex + 1);
-                            unset($listOfInterval[$position]);
+                            $newArray = array_merge($newArray, array_splice($listOfInterval, $startingIndex, $endingIndex - $startingIndex + 1));
 
+                            $uniqueArray = array_unique($newArray);
+                            $duplicates = array_diff_assoc($newArray, $uniqueArray);
+
+                            $listOfInterval = $resetList;
                         }
-                } else {
-                    $listOfInterval = $resetList;
-                }                
+                    }              
+                }
+                $listOfInterval = array_diff($listOfInterval, $duplicates);
+
+                sort($listOfInterval);
             }
         }
-        sort($listOfInterval);
+
+        if ($listOfInterval == []) {
+            return $this->render('reservation/reservationFull.html.twig', [
+                'places' => $places
+            ]);
+        }
+
+
+        
 
         $numberOfFreeTime = count($listOfInterval);
         return $this->render('reservation/reservationSearch.html.twig', [
@@ -130,26 +150,74 @@ class ReservationController extends AbstractController
     }
 
     #[Route('reservation/new', name: 'reservation_new')]
-    public function newReservation(EntityManagerInterface $entityManager, Request $request, )
+    public function newReservation(EntityManagerInterface $entityManager, Request $request, TableRepository $tableRepository, ReservationRepository $reservationRepository)
     {
         
         $data = json_decode($request->getContent(), true);
 
+        $places = $data['numberOfPeople'];
+
+        $tables = $tableRepository->searchByPlaces($places);
+
+        
         $date = $data['date'];
 
-        $date = new DateInterval($date);
+        $reservations = $reservationRepository->searchByDate($date, $places);
+
+        $dateTimeInterface = DateTimeImmutable::createFromFormat('Y-m-d', $date);
+
+        $hour = $data['hour'];
+
+        $hourTimeInterface = DateTimeImmutable::createFromFormat('H:i', $hour);
+
+        $hourTimeStamp = strtotime($data['hour']);
+
+
+        $interval = 900;
+
+        $tableId = null;
+
+
+        foreach ($tables as $table) {
+            $tableReservation = $table->getTableReservation();
+            if (count($tableReservation) == 0) {    
+                $tableId = $table->getId();
+            } else {
+                foreach ($reservations as $reservation) {
+                    $reservationTimeStamp = $reservation->getHour()->getTimestamp();
+                    if ( $reservation->getReservationTable()->getId() == $table->getId()){
+                        $reservationTimeStamp = $reservationTimeStamp + ($interval*3);
+                        if (  $hourTimeStamp >= $reservationTimeStamp && $hourTimeStamp <= $reservationTimeStamp ) {
+                            $tableId = $table->getId();
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($tableId == null) {
+            $randomKey = array_rand($tables);
+            $randomTable = $tables[$randomKey];
+
+            $tableId = $randomTable->getId();
+        }
+
+        $table = $tableRepository->find($tableId);
+
 
         $reservation = new Reservation();
         $reservation
-            ->setDate($date)
-            ->setHour($data['hour'])
+            ->setDate($dateTimeInterface)
+            ->setHour($hourTimeInterface)
             ->setNumberOfPeople($data['numberOfPeople'])
-            ->setReservationTable($data['reservationTable']);
+            ->setReservationTable($table);
 
         $entityManager->persist($reservation);
         $entityManager->flush();
 
-        return $this->render('reservationSucces.html.twig');
+        return $this->render('reservation/reservationSuccess.html.twig');
 
     }
+
+
 }
